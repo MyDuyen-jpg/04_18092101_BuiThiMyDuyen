451705cec0ab906dad725906333b73a919091a0c
MyDuyen-jpg <btmduyen121@gmail.com>
2021-10-15 17:11:58 +0700

    cau 6

--- app/src/main/java/com/myduyen/a04_18092101_buithimyduyen/MyService.java
+++ app/src/main/java/com/myduyen/a04_18092101_buithimyduyen/MyService.java
@@ -1,7 +1,7 @@
 package com.myduyen.a04_18092101_buithimyduyen;
 
 
-//import static com.myduyen.a04_18092101_buithimyduyen.MyAplication.CHANEL_ID;
+import static com.myduyen.a04_18092101_buithimyduyen.MyAplication.CHANEL_ID;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -16,63 +16,63 @@ import android.widget.RemoteViews;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
-public class MyService  {
-//    private Song mSong;
-//
-//
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//        Log.e("MY DUYEN","MyService onCreate");
-//    }
-//    @Nullable
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//        return null;
-//    }
-//
-//    @Override
-//    public int onStartCommand(Intent intent, int flags, int startId) {
-//        Bundle bundle = intent.getExtras();
-//        if(bundle != null){
-//            Song song = (Song) bundle.get("object_song");
-//
-//            if(song != null) {
-//                mSong = song;
-//                sendNotification(song);
-//            }
-//        }
-//        return START_NOT_STICKY;
-//    }
-//
-//    private void sendNotification(Song song) {
-//        Intent intent = new Intent(this, MainActivity.class);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), song.getImageSong());
-//
-//        RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.notification);
-//        remoteViews.setTextViewText(R.id.tv_title_song,song.getTitle());
-//        remoteViews.setTextViewText(R.id.tv_single_song,song.getSingle());
-//        remoteViews.setImageViewBitmap(R.id.img_song,bitmap);
-//        remoteViews.setImageViewResource(R.id.img_play_or_pause, R.drawable.ic_play);
-//        remoteViews.setImageViewResource(R.id.img_clear, R.drawable.ic_clear);
-//
-//
-//        // CHANEL_ID ghi r nhấn alt enter để import từ MyAplication
-//        Notification notification = new NotificationCompat.Builder(this,CHANEL_ID)
-//                .setSmallIcon(R.drawable.ic_notification)
-//                .setContentIntent(pendingIntent)
-//                .setCustomContentView(remoteViews)
-//                .setSound(null)
-//                .build();
-//
-//        startForeground(1, notification);
-//    }
-//
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        Log.e("MY DUYEN","MyService onDestroy");
-//    }
-}
+
+public class MyService extends Service { // extend của app.Service
+        private Song mSong;
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            Log.e("MD DUYEN", "MyService onCreate");
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Song song = (Song) bundle.get("object_song");
+
+                if (song != null) {
+                    mSong = song;
+                    sendNotification(song);
+                }
+            }
+            return START_NOT_STICKY;
+        }
+
+        private void sendNotification(Song song) {
+            Intent intent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), song.getImageSong());
+
+            RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification);
+            remoteViews.setTextViewText(R.id.tv_title_song, song.getTitle());
+            remoteViews.setTextViewText(R.id.tv_single_song, song.getSingle());
+            remoteViews.setImageViewBitmap(R.id.img_song, bitmap);
+            remoteViews.setImageViewResource(R.id.img_play_or_pause, R.drawable.ic_play);
+            remoteViews.setImageViewResource(R.id.img_clear, R.drawable.ic_clear);
+
+
+            // CHANEL_ID ghi r nhấn alt enter để import từ MyAplication
+            Notification notification = new NotificationCompat.Builder(this, CHANEL_ID)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setContentIntent(pendingIntent)
+                    .setCustomContentView(remoteViews)
+                    .setSound(null)
+                    .build();
+
+            startForeground(1, notification);
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Log.e("MY DUYEN", "MyService onDestroy");
+        }
+    }
